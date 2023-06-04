14f8d094af5fee9956641bcfaeb69d12d8af4d04
ryuki8643 <garyuki88me@gmail.com>
2023-06-04 23:54:54 +0900

    load file by glb

---
+++ src/BlenderComponent.tsx
@@ -0,0 +1,23 @@
+
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+import React from 'react';
+import { Dolphin } from './Dolphin';
+
+type BoxProps = {
+  position: [x: number, y: number, z: number];
+};
+
+const BlenderComponent = () => {
+  return (
+  <div style={{ width: '100vw', height: '100vh' }}>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Dolphin/>
+    </Canvas>
+  </div>
+)};
+
+export default BlenderComponent;
