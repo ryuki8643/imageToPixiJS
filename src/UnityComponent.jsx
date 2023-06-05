07543d904fcf29de59135797e31801ede3ff8ce3
ryuki8643 <garyuki88me@gmail.com>
2023-06-05 23:58:46 +0900

    add unity to move by react

--- src/UnityComponent.jsx
+++ src/UnityComponent.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function UnityComponent() {
-  const { unityProvider,unload } = useUnityContext({
+  const { unityProvider,unload,sendMessage } = useUnityContext({
     loaderUrl: "Build/Build.loader.js",
     dataUrl: "Build/Build.data",
     frameworkUrl: "Build/Build.framework.js",
@@ -11,13 +11,25 @@ function UnityComponent() {
 
   async function handleClickBack() {
     await unload();
-    // Ready to navigate to another page.
   }
+  const [x, setX] = React.useState(0);
+  const [y, setY] = React.useState(0);
+  const [z, setZ] = React.useState(0);  
+
+  useEffect(() => {
+
+      sendMessage("dolphin", "MoveObject", [x, y, z].join(","));
+
+  }, [x,y,z]);
+
 
   return (
     <>
       <Unity style={{ width: "100%", height: 300 }} unityProvider={unityProvider} />
       <button onClick={handleClickBack}>Unload</button>
+      <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
+      <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
+      <input type="number" value={z} onChange={(e) => setZ(e.target.value)} />
       </>
   );
 }
